acb105e174b41c09b50ef3c633b1747c9e7fea64
adamrizz <163289817+adamrizz@users.noreply.github.com>
2025-06-25 18:15:38 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 import requests
 import os
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 ROBOFLOW_API_URL = "https://detect.roboflow.com/penyakit-daun-padi-kumqp/1?api_key=gyZFta9jhWeEDO9nlnZo"
 
