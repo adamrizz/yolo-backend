8869229a527678c75b778db0a98850d25907b7d2
adamrizz <163289817+adamrizz@users.noreply.github.com>
2025-06-25 10:12:27 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -24,4 +24,5 @@ def predict():
     return jsonify(response.json())
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
